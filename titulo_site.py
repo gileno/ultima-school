e2861684f2b6599547064ea9b12206866f218988
Gileno Filho <contato@gilenofilho.com.br>
2022-07-12 19:58:24 -0300

    correção(titulo_site): melhorando a busca da posição do 'title'

--- titulo_site.py
+++ titulo_site.py
@@ -2,7 +2,7 @@ from requests_html import HTMLSession
 sessao = HTMLSession()
 site = input("Qual o site que deseja saber o título: ")
 resposta = sessao.get(site)
-tag_inicio = "<title"
-tag_fim = "</title>"
-titulo = resposta.text[resposta.text.index(tag_inicio):resposta.text.index(tag_fim)].replace("<title>", "")
+inicio_titulo = resposta.text.index("<title>")
+fim_titulo = resposta.text.index("</title>")
+titulo = resposta.text[inicio_titulo:fim_titulo].replace("<title>", "")
 print(titulo)
